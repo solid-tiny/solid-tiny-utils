4cfd2a2e1319447b8a345050863103a882a1d612
g-mero <gmerocom@gmail.com>
2025-12-27 03:27:30 +0800

    feat: remove on-motion-end feature

--- src/event/index.ts
+++ src/event/index.ts
@@ -3,7 +3,6 @@ export * from "./create-event-listener";
 export * from "./create-intersection-observer";
 export * from "./create-visibility-observer";
 export * from "./make-event-listener";
-export * from "./on-motion-end";
 
 import type { JSX } from "solid-js/jsx-runtime";
 
